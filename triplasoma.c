9e893283b323124c0269bb1ab007d3ed7d725f09
Elen Oliveira <elenoliveirafernandes@gmail.com>
2025-10-12 00:53:21 +0100

    triplasoma.c

--- triplasoma.c
+++ triplasoma.c
@@ -4,14 +4,15 @@ int main(){
     int N, res = 0, n1, n2, n3;
     scanf("%d", &N);
     
-    for(n1 = N; n1 >= 1; n1--){
+    for(n1 = N-2; n1 >= 1; n1--){
         for(n2 = n1; n2 >= 1; n2--){
-            for(n3 = n2; n3 >= 1; n3--){
-                if(n1 + n2 + n3 == N){
-                    res++;
-                }
+            n3 = N-n1-n2;
+            if(n3 >= 1 && n3<=n2){
+                res++;
+                //printf("%d+%d+%d\n", n1, n2, n3);
             }
         }
     }
+
     printf("%d\n", res);
 }
