33636a806ebc8d61b56f7b750d667e0202a6dd8a
Elen Oliveira <elenoliveirafernandes@gmail.com>
2025-10-16 23:26:30 +0100

    correções

--- euler.c
+++ euler.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int main(){
-    double fatK = 1.0, soma = 0.0;
+    double fatK = 1, res = 1;
     int termos;
     scanf("%d", &termos);
     for(int i = 0; i <= termos; i++){
-        for (int j = 1; j <= i; j++){
-            fatK *= j;
-        }
-        soma += 1/fatK;
+        if(!i)
+            continue;
+        fatK *= i;
+        res += (double)1/fatK;
     }
-    printf("%.16g", soma);
+    printf("%.16g", res);
 }
